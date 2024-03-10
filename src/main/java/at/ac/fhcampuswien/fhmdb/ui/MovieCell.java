66a66f93d612df9c1f93a981973ce52343fcbc40
gueney1 <gueney.arslan@stud.fh-campuswien.ac.at>
2024-03-10 14:27:23 +0100

    my version test

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -48,7 +48,7 @@ public class MovieCell extends ListCell<Movie> {
             }
 
             genre.setText(genresText.toString());
-            //testing
+            //testing1
 
             // color scheme
             title.getStyleClass().add("text-yellow");
