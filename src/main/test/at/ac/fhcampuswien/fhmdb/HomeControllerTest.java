324371b1ddcb986090c7597b01e6aec090d38637
iamwahida <nemati.w@yahoo.com>
2024-03-11 16:31:21 +0100

    second test done -> test if initialization of initializeMovies() is correct, every filme should have title, description, genre

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -17,12 +17,33 @@ public class HomeControllerTest {
 /*
 für Home-Controller
 Richtige Initialisierung der Filme in initializeMovies().
+ --> jeder Film in der Liste hat Titel, Beschreibung und Genre
+ --> Liste darf nicht leer sein
+
 Richtige Filtern und Sortieren der Filme in der HomeController-Klasse.
 
 für Movie:
 Ob die Erstellung neuer Movie-Objekte korrekt funktioniert.
 Ob die Getter-Methoden die richtigen Werte zurückgeben.
  */
+
+    @Test
+    void test_if_initialization_of_initializeMovies_is_correct() {
+        List<Movie> movies = Movie.initializeMovies();
+
+        assertFalse(movies.isEmpty(), "The list is not supposed to be empty.");
+
+        for (Movie movie : movies) {
+            assertNotNull(movie.getTitle(), "Titel should not be null.");
+            assertFalse(movie.getTitle().isEmpty(), "Titel should not be empty.");
+
+            assertNotNull(movie.getDescription(), "Description is not supposed to be null.");
+            assertFalse(movie.getDescription().isEmpty(), "Description should not be empty.");
+
+            assertNotNull(movie.getListGenres(), "Genre List should not be null.");
+            assertFalse(movie.getListGenres().isEmpty(), "Genre List should not be empty.");
+        }
+    }
 
     @Test
     void test_if_correct_movies_are_filtered_when_text_is_entered() {
@@ -38,4 +59,5 @@ Ob die Getter-Methoden die richtigen Werte zurückgeben.
             assertTrue(isInTitle || isInDescription, "Movie should contain the search text in its title or description.");
         }
     }
+
 }
